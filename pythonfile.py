7bd87f5dad11f29553e56fc6152ad4958f05ac30
aditya-b312 <adhikulal70@gmail.com>
2026-03-23 09:37:05 +0530

    new changes

--- pythonfile.py
+++ pythonfile.py
@@ -1,4 +1,4 @@
-# Multiplication table (from 1 to 10) in Python
+#Multiplication table (from 1 to 10) in Python
 
 num = 12
 
@@ -8,4 +8,3 @@ num = 12
 # Iterate 10 times from i = 1 to 10
 for i in range(1, 11):
    print(num, 'x', i, '=', num*i)
-
